976ab24ccd5b43a7a856ac47dd2bf94f5e707d1b
Jan Edwin Perez <janedwin2000@gmail.com>
2025-05-20 19:05:29 -0500

    Desarrollo para prueba en deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import Inicio from './pages/Inicio'
 
 
 const App = () => {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
